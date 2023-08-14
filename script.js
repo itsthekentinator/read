5f8d27c344ae2fdb40c1e10a3c2ad7287a184015
itsthekentinator <134373636+itsthekentinator@users.noreply.github.com>
2023-08-14 11:51:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // script.js
 function goToPlaylist() {
   // Replace this URL with the actual playlist URL
-  window.location.href = "https://www.example.com/reading-playlist";
+  window.location.href = "https://music.apple.com/jp/playlist/reading/pl.u-ZmblyVWs0eNKe6e?l=en-US";
 }
